2e43670e933203880a9ca13cd21edfc9afbab02f
deanlicious <dean.w1993@gmail.com>
2021-04-05 21:21:29 +0100

    adding punching shear

--- structureTK.py
+++ structureTK.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
 
 
 def kValue(M,b,d,fck):
@@ -80,44 +82,99 @@ def beamPlotDesign(startDepth,endHeight,b,M,fck):
     plt.plot(depthList,AsreqList)
     #plt.show()
 
-'''
-for b in range(300,1200,150):
-    beamPlotDesign(200,1600,b,300000000,35)
 
-plt.show()
-'''
-resolution = 20
-for p1 in [0.0016+((0.02-0.0016)/resolution)*i for i in range(resolution+1)]:
-    depthlist = []
-    pileCapacityList = []
-    for pileCapacity in range(500,10000,100):
-        pileCapacity = pileCapacity*10**3 #kN to N
+def plot_beam_no_links():
+    #plots the maximum capacity of a beam that doesn't require shear links
 
-        #run throught pilecapacity
-        pileSize = 750
-        length = pileSize*3
-        bw = pileSize + 300
-        Ned = pileCapacity*2
-        Ved = Ned/2
+    resolution = 20
+    for index, p1 in enumerate( [0.0016+((0.02-0.0016)/resolution)*i for i in range(resolution+1)]):
+        depthlist = []
+        pileCapacityList = []
+        for pileCapacity in range(500,10000,100):
+            pileCapacity = pileCapacity*10**3 #kN to N
+
+            #run throught pilecapacity
+            pileSize = 750
+            length = pileSize*3
+            bw = pileSize + 300
+            Ned = pileCapacity*2
+            Ved = Ned/2
+            
+            Med = (Ned*length)/400
+            for depth in range(200,5000,25):
+                beta = (pileSize*1.2)/(depth)
+                if beta > 1: beta =1
+                elif beta < 0.25: beta = 0.25
+                Ved2 = Ved*beta #shear inchancement
+                ved = Ved2/(0.9*bw*depth) #nts look into if z should be used if worst case!
+                vrc = ConcreteShearCapacity(35,depth,bw,p1*depth*bw)
+                #print(ved,vrc)
+                if vrc > ved:
+                    #print('Pilecapacity: ',pileCapacity,'  Depth: ',depth)
+                    depthlist.append(depth)
+                    pileCapacityList.append(pileCapacity*10**-3)
+                    break
+        #print(p1)
+        #print(pileCapacityList,depthlist)
+        if index == 0 or index == resolution:
+            col = '0.1'
+            
+        else:
+            col = '0.90'
+        if index == 0 or index == resolution or index%5 ==0:
+            plt.text(pileCapacityList[-1]+20,depthlist[-1]+ 20, str(p1*100)+'%('+
+                str(int(depth*bw*p1))+'mm²)'
+            )
+
+        lines = plt.plot(pileCapacityList,depthlist, color=col)
+
+        lines[0].set_linestyle( 'dashed')
         
-        Med = (Ned*length)/400
-        for depth in range(200,5000,25):
-            beta = (pileSize*1.2)/(depth)
-            if beta > 1: beta =1
-            elif beta < 0.25: beta = 0.25
-            Ved2 = Ved*beta #shear inchancement
-            ved = Ved2/(0.9*bw*depth)
-            vrc = ConcreteShearCapacity(35,depth,bw,p1*depth*bw)
-            #print(ved,vrc)
-            if vrc > ved:
-                #print('Pilecapacity: ',pileCapacity,'  Depth: ',depth)
-                depthlist.append(depth)
-                pileCapacityList.append(pileCapacity*10**-3)
-                break
-    #print(p1)
-    #print(pileCapacityList,depthlist)
-    plt.plot(pileCapacityList,depthlist)
-plt.show()
+    plt.show()
 
 
 
+class punchingshear():
+
+    def __init__(self):
+        self.width = 200
+        self.hight = 100
+        self.slapdepth = 250
+        self.concretegrade = 32
+        self.topcover = 30
+        self.bottomcover = 30
+        self.topreinforcement = [[16,200,0],[16,200,1],[20,200,1]] #[[diameter, spacing, layer]] = layer starts at 0
+
+    
+    def show(self):
+        
+        fig, ax = plt.subplots()
+        ax.plot([1,5,2],[2,3,4],color="cyan")
+        ax.add_patch(Rectangle((-self.width/2, -self.hight/2), self.width, self.hight, color="yellow"))
+        plt.xlabel("X-AXIS")
+        plt.ylabel("Y-AXIS")
+        plt.title("PLOT-1")
+        plt.show()
+
+    def uout(self):
+        pass
+
+    def effectivedepthcalulator(self):
+        maxRebarPerLayer = {} # {str(dia), layer}
+        for diamater, _ , layer in self.topreinforcement:
+            if str(layer) not in maxRebarPerLayer:
+                maxRebarPerLayer[str(layer)] = diamater
+            
+            else:
+                if maxRebarPerLayer[str(layer)] < diamater:
+                    maxRebarPerLayer[str(layer)] = diamater
+        
+        deff = self.slapdepth - self.topcover - sum(maxRebarPerLayer.values())
+
+        return deff
+
+
+col1 = punchingshear()
+print(col1.effectivedepthcalulator())
+
+print('done')
